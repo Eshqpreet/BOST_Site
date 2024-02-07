599b0ef5f36408a463ba5cebc7f1dbd7ec13c264
Rohan Pandya <rohanpandya07@gmail.com>
2024-02-07 12:09:15 +0530

    animation edit

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import HeroImage from "@/components/Hero-image";
 import Animation from "@/components/animation";
 import Testimonials from "@/components/Testimonials/testimonials";
 import ImageSlider from "@/components/ImageSlider/imageSlider";
+import classNames from "classnames";
+import { StarsIllustration } from "@/components/Icons/stars";
 
 export default function HomePage() {
   return (
@@ -28,22 +30,30 @@ export default function HomePage() {
             Some Glimpes of BOST
           </h2>
           <p className="text-[18px] leading-[30px] font-[400] text-primary-text  mt-[18px] text-center">
-          Embark on a Journey: A Captivating Glimpse into the Allure of BOST. Discover the Unseen, Experience the Extraordinary, and Immerse Yourself in the Vibrancy of BOST's Unique Charms.
+            Embark on a Journey: A Captivating Glimpse into the Allure of BOST.<br className="hidden md:block" /> Discover the Unseen, Experience the Extraordinary, and Immerse Yourself in the Vibrancy of BOST's Unique Charms.
           </p>
           <ImageSlider />
         </section>
 
         <section className="mt-10">
-          <div className="xl:w-[470px] mx-auto">
-            <h2 className="text-[39px] leading-[54px] font-[600] text-gradient text-center">
-              What People Says
+          <div className="text-center">
+            <h2 className="text-3xl mb-4 md:mb-7 md:text-6xl">
+              What People Say
             </h2>
-            <p className="text-[18px] leading-[30px] font-[400] text-primary-text  mt-[18px] text-center">
-              At BOST, it's not just education; it's an experience that speaks for itself. Students and visionaries alike echo the sentiment – BOST is where potential turns into achievement. The buzz is real, the excitement palpable. Discover what people say about us and step into a world where success has a voice – yours.
-            </p>
+            
+            <p className='text-primary-text mb-12 text-lg md:text-xl max-w-[68rem] mx-auto'>
+            At BOST, it's not just education; it's an experience that speaks for itself. Students and visionaries alike echo the sentiment – BOST is where potential turns into achievement. The buzz is real, the excitement palpable. Discover what people say about us and step into a world where success has a voice – yours.</p>
           </div>
           <Testimonials />
         </section>
+        <div className={classNames("h-[60rem] overflow-hidden mb-[-12.8rem] mt-[-1.8rem] relative mask-radial-faded pointer-index-none z-[-1]",
+        "before:bg-radial-faded [--color:#7877C6] before:absolute before:inset-0 before:opacity-[0.4]",
+        "after:absolute after:h-[142%] after:bg-background after:rounded-[50%] after:border-[rgba(120,_119,_198,_0.4)] after:border-t after:top-1/2 after:-left-1/2 after:w-[200%]"
+      )}>
+        <div className="mr-[20px]">
+          <StarsIllustration />
+        </div>
+      </div>
 
 
       </Container>
