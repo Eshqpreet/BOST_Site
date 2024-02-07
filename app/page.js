b124426ab19f7a16cfddb795d4ed18b1f7b32231
Eshqpreet Singh <eshqpreet@gmail.com>
2024-02-07 12:31:28 +0530

    improved

--- app/page.js
+++ app/page.js
@@ -22,38 +22,39 @@ export default function HomePage() {
             <span className="font-semibold md:text-[35px] text-[20px]">BOST IIT Ropar</span>
           </HeroSubtitle>
         </Hero>
+
       </Container>
       <Container>
 
-        <section className="mt-3">
-          <h2 className="text-[39px] leading-[54px] font-[600] text-gradient text-center">
+        <section className="mt-5">
+          <h2 className="text-[30px] leading-[54px] font-[600] text-gradient text-center">
             Some Glimpes of BOST
           </h2>
-          <p className="text-[18px] leading-[30px] font-[400] text-primary-text  mt-[18px] text-center">
+          {/* <p className="text-[18px] leading-[30px] font-[400] text-primary-text  mt-[18px] text-center">
             Embark on a Journey: A Captivating Glimpse into the Allure of BOST.<br className="hidden md:block" /> Discover the Unseen, Experience the Extraordinary, and Immerse Yourself in the Vibrancy of BOST's Unique Charms.
-          </p>
+          </p> */}
           <ImageSlider />
         </section>
 
-        <section className="mt-10">
+        <section className="mt-[10rem]">
           <div className="text-center">
             <h2 className="text-3xl mb-4 md:mb-7 md:text-6xl">
               What People Say
             </h2>
-            
+
             <p className='text-primary-text mb-12 text-lg md:text-xl max-w-[68rem] mx-auto'>
-            At BOST, it's not just education; it's an experience that speaks for itself. Students and visionaries alike echo the sentiment – BOST is where potential turns into achievement. The buzz is real, the excitement palpable. Discover what people say about us and step into a world where success has a voice – yours.</p>
+              At BOST, it's not just education; it's an experience that speaks for itself. Students and visionaries alike echo the sentiment – BOST is where potential turns into achievement. The buzz is real, the excitement palpable. Discover what people say about us and step into a world where success has a voice – yours.</p>
           </div>
           <Testimonials />
         </section>
         <div className={classNames("h-[60rem] overflow-hidden mb-[-12.8rem] mt-[-1.8rem] relative mask-radial-faded pointer-index-none z-[-1]",
-        "before:bg-radial-faded [--color:#7877C6] before:absolute before:inset-0 before:opacity-[0.4]",
-        "after:absolute after:h-[142%] after:bg-background after:rounded-[50%] after:border-[rgba(120,_119,_198,_0.4)] after:border-t after:top-1/2 after:-left-1/2 after:w-[200%]"
-      )}>
-        <div className="mr-[20px]">
-          <StarsIllustration />
+          "before:bg-radial-faded [--color:#7877C6] before:absolute before:inset-0 before:opacity-[0.4]",
+          "after:absolute after:h-[142%] after:bg-background after:rounded-[50%] after:border-[rgba(120,_119,_198,_0.4)] after:border-t after:top-1/2 after:-left-1/2 after:w-[200%]"
+        )}>
+          <div className="mr-[20px]">
+            <StarsIllustration />
+          </div>
         </div>
-      </div>
 
 
       </Container>
--- components/Testimonials/testimonials.jsx
+++ components/Testimonials/testimonials.jsx
@@ -33,51 +33,51 @@ const Testimonials = () => {
                         spaceBetween: 30
                     }
                 }}>
-                <SwiperSlide> <div className='py-[30px] px-5 rounded-3 hi'>
+                <SwiperSlide> <div className='py-[30px] px-5 rounded-xl bg-glass-gradient'>
                     <div className='flex items-center justify-between'>
                         <Image src={img1} alt="img" className='rounded-[50%] w-[80px] h-[80px]' />
                         <div>
                             <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor '>
                                 Dr. S. C. Martha
                             </h4>
-                            <p className='text-[13px] leading-[30px] font-semibold text-headingColor '>
+                            <p className='text-[13px] leading-[30px] font-semibold text-primary-text '>
                                 Dean Student Affairs
                             </p>
                         </div>
                     </div>
-                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>
+                    <p className='text-[16px] leading-9 mt-4 text-textColor font-[400] text-primary-text'>
                         What I understand the duty of student affairs (SA) is to create an environment to students to participate in various academic and non-academic activities, while judiciously ensuring discipline and security, for making them global "learned" leaders with utmost satisfaction on their understanding, knowledge and experience gained while staying at campus. I strongly believe that, our students have huge potential to become world leaders and SA team is working hard to ensure this happen !
                     </p>
                 </div></SwiperSlide>
-                <SwiperSlide>  <div className='py-[30px] px-5 rounded-3 hi'>
+                <SwiperSlide>  <div className='py-[30px] px-5 rounded-xl bg-glass-gradient'>
                     <div className='flex items-center justify-between'>
                         <Image src={img2} alt="img" className='rounded-[50%] w-[80px] h-[80px]' />
                         <div>
                             <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor '>
                                 Dr. Ashwani Sharma
                             </h4>
-                            <p className='text-[13px] leading-[30px] font-semibold text-headingColor '>
+                            <p className='text-[13px] leading-[30px] font-semibold text-primary-text'>
                                 Faculty Advisor, BOST
                             </p>
                         </div>
                     </div>
-                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>
+                    <p className='text-[16px] leading-9 mt-4 text-primary-text font-[400]'>
                         Own midst. Behold sea created male he together of That Said fourth deep abundantly have light night beginning rule darkness seed darkness which land saying moveth. Fifth shall wont signs, can seasons green days gathered great
                     </p>
                 </div></SwiperSlide>
-                <SwiperSlide><div className='py-[30px] px-5 rounded-3 hi'>
+                <SwiperSlide><div className='py-[30px] px-5 rounded-xl bg-glass-gradient'>
                     <div className='flex items-center justify-between'>
                         <Image src={img3} alt="img" className='rounded-[50%] w-[80px] h-[80px]' />
                         <div>
                             <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor '>
                                 Mehul Gupta
                             </h4>
-                            <p className='text-[13px] leading-[30px] font-semibold text-headingColor '>
+                            <p className='text-[13px] leading-[30px] font-semibold text-primary-text '>
                                 General Secretary, BOST
                             </p>
                         </div>
                     </div>
-                    <p className='text-[16px] leading-7 mt-4 text-[textColor] font-[400]'>
+                    <p className='text-[16px] leading-9 mt-4 font-[400] text-primary-text'>
                         BOST is a place for dreamers,innovators and out of the box thinkers with various club here guided by enthusiastic people one can learn ,innovate , build and own the future. To be a leader in Science and Technology we seek your support and participation in this strive of ours. So pick up a club which fascinates you and join with us!
                     </p>
                 </div></SwiperSlide>
